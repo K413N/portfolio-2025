094a54d119e0c8ad604c54e945ae9afa267d9dc9
K413N <Kalen.Coolson@gmail.com>
2025-10-11 14:08:45 -0400

    styled navbar component skeleton

--- src/app/components/Navbar/Navbar.js
+++ src/app/components/Navbar/Navbar.js
@@ -1,14 +1,41 @@
-import React from 'react'
-import styles from "../../styles"
+'use client'
+import React from "react";
+import styled from "styled-components";
+
+const NavWrapper = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: stretch;
+  flex-direction: row;
+  background-color: #1a1b2bff;
+  padding: 12px;
+  border-top-color: #ffffff;
+  border-bottom-color: #1a1b2bff;
+  outline: solid;
+  outline-offset: 4px;
+  outline-width: 2px;
+  border-radius: 32px;
+
+`
+
+const NavButton = styled.button`
+  display: flex;
+  background: transparent;
+  border-radius: 3px;
+  border: 2px solid #bf4f74;
+  color: #bf4f74;
+  margin: 0 1em;
+  padding: 0.25em 1em;
+`;
 
 function Navbar() {
-    return (
-        <div className="navbar">
-            <button className="navbutton">Home</button>
-            <button className="navbutton">About me</button>
-            <button className="navbutton">Contact me</button>
-        </div>
-    )
+  return (
+    <NavWrapper>
+      <NavButton className="navbutton">Home</NavButton>
+      <NavButton className="navbutton">About me</NavButton>
+      <NavButton className="navbutton">Contact me</NavButton>
+    </NavWrapper>
+  );
 }
 
 export default Navbar;
