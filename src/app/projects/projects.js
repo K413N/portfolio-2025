47bf64cd0b1814aff1cf43a78c8a3af8dba1284b
K413N <kalen.coolson@gmail.com>
2026-01-10 09:54:16 -0500

    added projects area and fixed a few routing issues

---
+++ src/app/projects/projects.js
@@ -0,0 +1,23 @@
+
+export async function getProjectInfo(projectId) {
+    return PROJECT_DATA(projectId)
+}
+
+const PROJECT_DATA = {
+    beatclash: {
+        name: 'Beatclash',
+        description: 'A forumn I made using reactplayer, and mongodb.',
+    },
+    proceduralnightmares: {
+        name: 'Procedural Nightmares',
+        description: 'My frist released game on itch.io!',
+    },
+    gunbunnii: {
+        name: 'GunBunnii',
+        description: 'An FPS game I am working on',
+    },
+    procgen: {
+        name: 'procedural generation demo',
+        description: 'A small test area I did to understand procedural generation.',
+    },
+}
